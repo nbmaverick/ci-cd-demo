dad585cacd1163055165df305618435e3da158e9
Nursultan Baitenov <120434537+nbmaverick@users.noreply.github.com>
2023-04-03 20:45:50 -0500

    added actions

--- asg/backend.tf
+++ asg/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "internal1234567"
-    key    = "path/to/my/vpc"
+    key    = "path/to/my/asg"
     region = "us-east-1"
   }
 }
--- rds/backend.tf
+++ rds/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "internal1234567"
-    key    = "path/to/my/vpc"
+    key    = "path/to/my/rds"
     region = "us-east-1"
   }
 }
